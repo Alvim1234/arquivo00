ff224da2460bf9b43c19d28e3a169d4f50f6b188
Alvim1234 <lucasalvimetec@gmail.com>
2025-08-08 10:42:52 -0300

    Alimentação das classes

--- src/ClassePessoas.java
+++ src/ClassePessoas.java
@@ -1,4 +1,26 @@
 
 public class ClassePessoas {
-
+	String nomeAt;
+	String documentoAt;
+	float rendaAt;
+	
+	public ClassePessoas() {
+		
+	}
+	
+	public ClassePessoas(String nomePar, String documentoPar, float rendaPar) {
+		this.nomeAt = nomePar;		
+		this.documentoAt = documentoPar;
+		this.rendaAt = rendaPar;
+	}
+	public void alterarNome(String nomePar) {
+		this.nomeAt = nomePar;
+		
+	}
+	public void alterarDocumento(String documentoPar) {
+		this.documentoAt = documentoPar;
+	}
+	public void alterarRenda(float rendaPar) {
+		this.rendaAt = rendaPar;
+}
 }
--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -1,4 +1,10 @@
 
 public class ClassePrincipal {
-
+	public static void main(String[] args) {
+		
+		ClassePessoas obj1 = new ClassePessoas();
+		ClassePessoas obj2 = new ClassePessoas("Ronaldo", "123", 2500);
+		
+		
+	}
 }
